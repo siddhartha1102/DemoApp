e69797b51d19c0a044760fad116cf0ec31bf08a6
siddhartha1102 <58029800+siddhartha1102@users.noreply.github.com>
2022-04-25 19:12:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+st.title("D Siddhartha")
+st.subheader('''B6-204, L&T South City, Arekere Mico Layout, Bangalore – 560076
+Phone: +91 7760517242,\n
+ Email: siddharthadronamraju@gmail.com
+''')
+with st.expander("Objective "):
+    st.write("""
+Final Year MCA (Master of Computer application) student seeking an Entry-level position in 
+the field of information technology with emphasis on Product Development and Research
+    """)
+
